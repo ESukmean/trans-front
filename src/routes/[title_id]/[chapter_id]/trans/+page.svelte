3970b0fd8d8fb06326b3edb9a4ee537c30597350
ESukmean <esukmean@esukmean.com>
2024-12-21 18:36:47 +0900

    번역쪽 편집 UI 생성 및 개선

--- src/routes/[title_id]/[chapter_id]/trans/+page.svelte
+++ src/routes/[title_id]/[chapter_id]/trans/+page.svelte
@@ -5,7 +5,7 @@
     import type { TransLine } from "../+page.server";
 
 
-    beforeNavigate((nav) => chapterName.length + chapterLine.length > 0 ? nav.cancel() : {})
+    beforeNavigate((nav) => chapterLines.length > 0 ? nav.cancel() : {})
 
     const { data } = $props();
     const chapter = data.article;
@@ -15,10 +15,9 @@
     data.list.forEach(([k, v]) => {
         linesOrignal[k] = v[0]
     })
-    let linesOrignalSorted = Object.entries(linesOrignal).toSorted((a, b) => parseInt(a[0]) - parseInt(b[0])).map(([_, v]) => v)
+    const linesOrignalSorted = Object.entries(linesOrignal).toSorted((a, b) => parseInt(a[0]) - parseInt(b[0])).map(([_, v]) => v)
 
     interface SnapshotData {
-        chapterNameSnapshot: string;
         chapterLineSnapshot: String;
         chapterLinesSnapshot: string[];
     }
@@ -26,17 +25,14 @@
     export const snapshot: Snapshot<SnapshotData> = {
         capture: () => {
             return {
-                chapterNameSnapshot: chapterName,
                 chapterLineSnapshot: chapterLine,
                 chapterLinesSnapshot: chapterLines,
             };
         },
         restore: ({
-            chapterNameSnapshot,
             chapterLineSnapshot,
             chapterLinesSnapshot,
         }) => {
-            chapterName = chapterNameSnapshot;
             chapterLineSnapshot = chapterLineSnapshot;
             chapterLinesSnapshot = chapterLinesSnapshot;
         },
@@ -80,11 +76,8 @@
         return html.replace(/<(?!br\s*\/?)[^>]+>/g, '');
     }
 
-    function eventConvertLine() {
-        chapterLines = splitLine(chapterLine);
-    }
-    function eventApplyPostProcess() {
-        chapterLines = splitPostProcess(chapterLines);
+    function doAutoTrans() {
+        
     }
     function eventPreviewInput(e: InputEvent) {
         if (e.inputType != "insertFromPaste") {
@@ -93,22 +86,20 @@
 
         e.preventDefault();
 
-        const target = e.target! as HTMLElement
+        const target = e.target! as HTMLTextAreaElement
         const targetIdx =  parseInt(target.dataset.idx!);
 
-
-        target.innerHTML = stripHtml(target.innerHTML)
-        const splited = splitLine(target.innerText)
+        const splited = splitLine(target.value)
 
 
         chapterLines = chapterLines.toSpliced(targetIdx + 1, 0, ...splited.filter((_, idx) => idx >= 1))
         chapterLines[targetIdx] = splited[0];
     }
     function eventPreivewKeyDown(e: KeyboardEvent) {
-        const target = e.target! as HTMLElement;
+        const target = e.target! as HTMLTextAreaElement;
         const targetIdx =  parseInt(target.dataset.idx!);
-        
-        const cur = window.getSelection()
+        const curStart = target.selectionStart;
+        const curEnd = target.selectionEnd;
 
         switch (e.key) {
             case 'Backspace':
@@ -118,33 +109,29 @@
                     return;
                 }
 
-                if (cur?.anchorOffset == 0) {
+                if (curStart != curEnd ) {
+                    return;
+                }
+
+                if (curStart == 0) {
                     if (targetIdx == 0) { return }
                     e.preventDefault()
 
-                    chapterLines[targetIdx - 1] = chapterLines[targetIdx - 1] + " " + target.innerText
+                    chapterLines[targetIdx - 1] = chapterLines[targetIdx - 1] + " " + target.value
                     chapterLines = chapterLines.toSpliced(targetIdx, 1)
                 }
                 break;
             
             case 'Enter':
                 e.preventDefault();
-                if (target.textContent?.length == 0) {
+                if (target.value.length == 0) {
                     chapterLines = chapterLines.toSpliced(targetIdx + 1, 0, '');
                     return;    
                 }
-                if (!cur || cur.rangeCount == 0) return;
-
-                const range = cur.getRangeAt(0);
-                const parentElement = range.commonAncestorContainer.parentNode;
-
-                if (parentElement !== target) {
-                    return;
-                }
 
                 // Split the content at the cursor position
-                const textBefore = target!.textContent!.slice(0, range.startOffset);
-                const textAfter = target!.textContent!.substring(range.startOffset);
+                const textBefore = target!.value.slice(0, curStart);
+                const textAfter = target!.value.substring(curEnd);
                 
                 chapterLines = chapterLines.toSpliced(targetIdx, 0, textBefore)
                 chapterLines[targetIdx + 1] = textAfter;
@@ -164,14 +151,23 @@
                 return "\\u" + char.charCodeAt(0).toString(16).padStart(4, "0");
             });
         }
+        function mappingChapterLineWithOriginal() {
+            let data: {[key: number]: string} = {}
 
-        const postObj = {
-            name: escapeString(chapterName),
-            lines: chapterLines.map(escapeString),
-            info: escapeString(chapterInfo)
+            chapterLines.forEach((v, i) => {
+                data[linesOrignalSorted[i].id.lineNo] = v
+            });
+
+            return data
         }
 
-        fetch(`http://localhost:8080/api/${title.id}/`, {
+        const postObj = {
+            lines: mappingChapterLineWithOriginal(),
+            info: escapeString(chapterInfo),
+            transType: transType
+        }
+
+        fetch(`http://localhost:8080/api/${title.id}/${chapter.id}/`, {
             body: JSON.stringify(postObj),
             method: 'POST',
             headers: {
@@ -187,19 +183,16 @@
         })
     }
 
-    let chapterName = $state("");
+    let transType = $state('1');
     let chapterLine = $state("");
-    let chapterLines: string[] = $state([]);
+    let chapterLines: string[] = $state(linesOrignalSorted.map(v => v.line));
     let chapterInfo = $state("");
+
 
     let removeTailingLine: boolean | null = $state(null);
 </script>
 
-<style>
-    /* :global(.editline br) {
-        display: none
-    } */
-</style>
+
 <Frame>
     <form class="mx-auto px-12 py-6" method="post" onsubmit="{eventFormSubmit}">
         <header class="text-2xl font-bold py-2 mb-6">{title.title}<small class="block text-slate-500">{chapter.id} - {chapter.chapterTitle}</small></header>
@@ -208,41 +201,25 @@
                 <div class="flex-1">
                     <div class="p-2 border rounded">
                         <span class="block text-xl font-bold py-2 block mb-4"
-                            >🛠️ 줄 바꾸기</span
+                            >🛠️ 자동 번역</span
                         >
-                        <div class="font-bold">표시 문장 설정</div>
-                        <div class="p-2 mb-4">
-                            <label class="block"
-                                ><input
-                                    type="checkbox"
-                                    bind:checked={removeTailingLine}
-                                /> 따라붙는 줄 지우기</label
-                            >
-                        </div>
-                        <div class="flex gap-2">
+                        <select class="block font-bold text p-2 py-1 w-full">
+                            <option value="1">번역: ChatGPT</option>
+                            <option value="2">번역: Claude</option>
+                        </select>
+                        <textarea bind:value={chapterInfo} class="w-full h-40 border rounded mt-2 block p-1" placeholder="GPT가 번역할 때 사용할 추가정보를 입력해 주세요 (영어 권장)"></textarea>
+                        <div class="mt-2">
                             <button
-                                class="block w-full rounded border border-slate-200 bg-slate-200 hover:bg-slate-700 p-2 text-center hover:text-white"
-                                onclick={eventApplyPostProcess}
+                                class="block w-32 rounded border border-emerald-200 bg-emerald-700/80 hover:bg-emerald-700 p-2 text-center text-white"
+                                onclick={doAutoTrans}
                                 type="button"
                             >
-                                지금 상태에서 적용
-                            </button>
-                            <button
-                                class="block w-full rounded border border-emerald-200 bg-emerald-700/80 hover:bg-emerald-700 p-2 text-center text-white"
-                                onclick={eventConvertLine}
-                                type="button"
-                            >
-                                줄 변환
+                                자동 번역
                             </button>
                         </div>
                     </div>
                 </div>
-                <div class="p-2 border rounded">
-                    <span class="block text-xl font-bold py-2 block mb-4"
-                        >📄 번역 메모 (GPT 전달됨)</span
-                    >
-                    <textarea bind:value={chapterInfo} class="w-full h-40 border rounded" placeholder="GPT가 번역할 때 사용할 추가정보를 입력해 주세요 (영어 권장)"></textarea>
-                </div>
+    
                 <div class="flex-1 mt-4">
                     <button
                         class="block rounded border border-rose-200 bg-orange-500/80 hover:bg-orange-700 p-2 text-center text-white w-full"
@@ -258,7 +235,7 @@
                         </span>
                     </div>
                     <div class="flex-1 block">
-                        <select class="block font-bold text p-2 py-1">
+                        <select class="block font-bold text p-2 py-1" bind:value={transType}>
                             <option value="1">번역: ChatGPT</option>
                             <option value="2">번역: Claude</option>
                         </select>
@@ -266,16 +243,19 @@
                 </div> 
                 <div>
                     <div class="border min-h-20 rounded">
-                        {#each linesOrignalSorted.map(v => [v, 'aaaa']) as line}
+                        {#each linesOrignalSorted.map((v, i) => [v, chapterLines[i] ?? '', i]) as line}
                             <div class="flex bg-slate-100 m-2">
-                                <div class="p-2 min-h-10 flex-1 w-2/4" data-idx={line[0].id.lineNo}>
+                                <div class="p-2 min-h-10 flex-1 w-2/4 {(line[0].line.trim().length == 0) ? 'bg-amber-100' : 'bg-violet-100'}" data-idx={line[0].id.lineNo}>
                                     {line[0].line}
                                 </div>
                                 <textarea
                                     contenteditable="true"
                                     class="p-2 min-h-10 flex-1 w-2/4 {(line[0].line.trim().length == 0 && line[1].trim().length > 0 || line[0].line.trim().length > 0 && line[1].trim().length == 0) ? 'bg-rose-100' : 'bg-violet-100'}"
-                                    data-idx={line[0].id.lineNo}
-                                    bind:value={line[1]}
+                                    data-idx={line[2]}
+                                    data-match-idx={line[0].id.lineNo}
+                                    onkeydown={eventPreivewKeyDown}
+                                    oninput={eventPreviewInput}
+                                    bind:value={chapterLines[line[2]]}
                                 >
                                 </textarea>
                             </div>
